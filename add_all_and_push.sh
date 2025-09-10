f0ece79d156c54cee72ef4896afcde770a73e677
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-09-10 16:40:36 +0200

    _

--- add_all_and_push.sh
+++ add_all_and_push.sh
@@ -4,7 +4,6 @@ read -p "Commit message: " message
 
 # It is very important to clean the repo before pushing, it makes
 # the push size significantly smaller
-mvn clean
 
 git add . && git commit -m "$message" && git push
 
