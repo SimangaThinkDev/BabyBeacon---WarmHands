ae46ea62d12dd787c00de26b7cc6f3049746ba63
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-09-10 12:01:44 +0200

    new repo who's this

--- MoreMove/asgi.py
+++ MoreMove/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for baby_beacon project.
+ASGI config for MoreMove project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baby_beacon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoreMove.settings')
 
 application = get_asgi_application()
